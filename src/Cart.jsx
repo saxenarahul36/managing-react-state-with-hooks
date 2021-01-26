92eb0a9d326ba846d46a37b4849faf5da76e7823
Rahul saxena <saxenarahul36@gmail.com>
2021-01-26 22:25:05 +0800

    add to cart page, manage state

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import React from "react";
+import useFetchAll from "./services/useFetchAll";
+import Spinner from "./Spinner";
 
+export default function Cart({ cart, updateQuantity, removeItems }) {
+  const urls = cart.map((i) => `products/${i.id}`);
+  const { data: products, loading, error } = useFetchAll(urls);
 
-export default function Cart (){
-    return  <h1>Cart</h1>
+  function renderItem(itemInCart) {
+    debugger;
+    const { id, sku, quantity } = itemInCart;
+    const { price, name, image, skus } = products.find(
+      (p) => p.id === parseInt(id)
+    );
+    const { size } = skus.find((s) => s.sku === sku);
 
+    return (
+      <li key={sku} className="cart-item">
+        <img src={`/images/${image}`} alt={name} />
+        <div>
+          <h3>{name}</h3>
+          <p>${price}</p>
+          <p>Size: {size}</p>
+          <p>
+            <select
+              aria-label={`Select quantity for ${name} size ${size}`}
+              onChange={(e) => updateQuantity(sku, parseInt(e.target.value))}
+              value={quantity}
+            >
+              <option value="0">Remove</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </p>
+        </div>
+      </li>
+    );
+  }
+
+  if (loading) return <Spinner />;
+  if (error) throw error;
+  const totalItemsInCart = cart.reduce((total,currentItem)=> total+ currentItem.quantity,0)
+
+  return (
+    <section id="cart">
+      <h1>Cart</h1>
+      <h3>
+        {totalItemsInCart === 0 ? 'Your cart is empty' :` Total number of items in cart :${totalItemsInCart}`}
+      </h3>
+      <ul>{cart.map(renderItem)}</ul>
+    </section>
+  );
 }
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React , {useState }from "react";
 import Products from "./Product";
+import { useParams } from "react-router-dom";
+import useFetch from "./hooks/useFetch";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
 
-export default function Details(props) {
-  const { product } = props;
+export default function Details({ addToCart }) {
+  const navigation = useNavigate();
+  const { id } = useParams();
+  const [size, setSize] = useState("");
+  const { loading, data: product, error } = useFetch(`products/${id}`);
+  if (loading) return <Spinner></Spinner>;
+  if (product.length === 0) return <PageNotFound />;
+  if (error) throw error;
+  const setSizeHandler = (e) => {
+    setSize(e.target.value);
+  };
   return (
     <div id="details">
-      <h1>{"Details "}</h1>
-      <p>{product?.descriptions}</p>
-      <p>{product?.price}</p>
-      <img src={`/image/${product?.image}`} alt={product?.category}></img>
+      <h1>{product?.name}</h1>
+      <p>{product?.description}</p>
+      <p>${product?.price}</p>
+
+      <section id="filters">
+        {/* <label htmlFor="size">Filter by Size:</label>{" "} */}
+        <select id="size" value={size} onChange={setSizeHandler}>
+          <option value="">Whats size ?</option>
+          {product.skus.map((item) => {
+            return (
+              <option key={item.size} value={item.sku}>
+                {item.size}
+              </option>
+            );
+          })}
+        </select>
+        {/* {size ? <h2>Found {filteredProduct.length} items</h2> : null} */}
+      </section>
+      <p>
+        <button
+          disabled={!size}
+          className="btn btn-primary"
+          onClick={() => {
+            addToCart(id,size)
+            navigation("/cart");
+          }}
+        >
+          Add to cart
+        </button>
+      </p>
+
+      <img src={`/images/${product?.image}`} alt={product?.category}></img>
     </div>
   );
 }
